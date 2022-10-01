4ced9add50ec9297b4e3a6c36d1c87bd11b3ef1c
Jacob Vetere <jvetere@aohwv.com>
2022-10-01 11:49:20 -0400

    Added files for footer, blog links and blogs themselves. Begun blog cards Begun blog rest service

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import RouterComp from './Routes/Router';
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 function App() {
   return (
     <div className="App">
-      
       <RouterComp/>
     </div>
   );
