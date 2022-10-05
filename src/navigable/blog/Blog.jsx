66b23d0dae76dc19ae1f790aa99031468f7c7995
Jacob Vetere <jvetere@aohwv.com>
2022-10-05 19:22:15 -0400

    Working through axios autoload mostly working through JPA
    Current state reciving from DB
    
    TODO: Parse response into PostCard

--- src/navigable/blog/Blog.jsx
+++ src/navigable/blog/Blog.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import PostCard from './components/PostCard'
+import {React, Component} from 'react';
+import PostCard from './components/PostCard';
 import axios from 'axios';
 
 /**
@@ -14,14 +14,30 @@ class Blog extends Component {
      * Axios Request to Spring Server on mount
      */
     componentDidMount() {
-        axios.get("localhost:8080/blogs")
+        axios.get("http://localhost:8080/blogs")
         .then((response) => {
-            this.setState({
-                blogs: response.data.blogs
-            })
+            // this.setState({
+            //     blogs: response.data.blogs
+            // })
+            console.log(response.data)
         })
     }
 
+    render() {
+        return (
+            <div className='BlogPreviewPage'>
+            {
+                this.state.blogs.map((post) => {
+                    return (
+                        <PostCard
+                            post={post}
+                        />
+                    )
+                })
+            }
+            </div> 
+        )
+    }
     
 }
 export default Blog;
