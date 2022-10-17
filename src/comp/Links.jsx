eb94857678a718fa2afa483f9906355bd11af8b0
Jacob Vetere <jvetere@aohwv.com>
2022-10-17 18:56:30 -0400

    FIxed service dropdown being green

--- src/comp/Links.jsx
+++ src/comp/Links.jsx
@@ -21,10 +21,10 @@ function Links() {
         </Nav.Link> */}
         <NavDropdown variant="dark" title="SERVICES" id="basic-nav-dropdown">
             <NavDropdown.Item>
-                <Link to="/overview">SERVICE</Link>
+                <Link to="/overview" style={{'color':'black'}}>SERVICE</Link>
             </NavDropdown.Item>
-            <NavDropdown.Item><Link to="/pro_services" style={{'color':'black'}}>Professional Services</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to="/managed_services" style={{'color':'black'}}>Managed Services</Link></NavDropdown.Item>
+            <NavDropdown.Item><Link to="/pro_services" style={{'color':'black'}}>PROFESSIONAL SERVICES</Link></NavDropdown.Item>
+            <NavDropdown.Item><Link to="/managed_services" style={{'color':'black'}}>MANAGED SERVICES</Link></NavDropdown.Item>
         </NavDropdown>
         {/* <Nav.Link><Link to="/store">STORE</Link></Nav.Link> */}
         <Nav.Link><Link to="/blog">BLOG</Link></Nav.Link>
